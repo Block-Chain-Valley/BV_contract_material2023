abef53c32dd45150d7829a48a49fc9db4c380fb8
진수하 <jinsuha@jinsuhaui-MacBookAir.local>
2023-03-03 21:56:10 +0900

    Part1-3 upload

---
+++ Part1-3/contracts/BummyOwnership.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "./BummyBase.sol";
+
+
+contract BummyOwnership is BummyBase {
+
+    /// @dev _tokenId에 해당하는 token의 owner가 _claimant 과 동일하면 true 아니면 false를 반환
+    /// @param _claimant the address we are validating against.
+    /// @param _tokenId kitten id, only valid when > 0
+    function _owns(address _claimant, uint256 _tokenId) internal view returns (bool) {
+        
+    }
+
+    /// @dev 사용자가 잘못된 주소(BummyCore)로 보냈을 때, 다시 사용자에게 돌려주기 위해 필요함.
+    /// _bummyId에 해당하는 token이 현재 이 컨트랙트에 있는지 확인해야 합니다.
+    /// 있다면 이를 _recipient한테 돌려보냅니다.
+    /// @param _bummyId - ID of bummy
+    /// @param _recipient - Address to send the cat to
+    function rescueLostBummy(uint256 _bummyId, address _recipient) public onlyCOO whenNotPaused {
+        
+    }
+}
